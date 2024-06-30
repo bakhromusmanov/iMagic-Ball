715379a0afc586a964fd5d5791c629efca62c38b
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-06-30 08:40:47 +0500

    Initial Commit

---
+++ iMagic Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iMagic Ball
+//
+//  Created by MacBook Air on 30/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
